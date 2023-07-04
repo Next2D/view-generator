eddcb42f75739e75cd0085ddd9a72ecbb2fc6621
ienaga <ienaga@tvon.jp>
2023-07-04 23:17:32 +0900

    TypeScriptの書き出し処理を追加

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const fs          = require("fs-extra");
 /**
  * @return {void}
  */
-const createFile = function ()
+const createJavaScriptFile = function ()
 {
 
     console.log();
@@ -146,6 +146,131 @@ export class ${name}ViewModel extends ViewModel
 /**
  * @return {void}
  */
+const createTypeScriptFile = function ()
+{
+
+    console.log();
+    console.log(`${chalk.green("Begin research.")}`);
+    console.log();
+
+    const cwd = `${process.cwd()}/src/`;
+
+    const routingPath = path.join(cwd, "config/routing.json");
+    if (!fs.existsSync(routingPath)) {
+        console.log(`${chalk.red("Could not find routing.json.")}`);
+        console.log();
+        return ;
+    }
+
+    const routing = require(routingPath);
+    const keys = Object.keys(routing);
+
+    for (let idx = 0; idx < keys.length; ++idx) {
+
+        const names = keys[idx].split("-");
+
+        if (names[0].charAt(0) === "@") {
+            continue;
+        }
+
+        const viewDir = path.join(cwd, `view/${names[0]}`);
+        if (!fs.existsSync(viewDir)) {
+
+            console.log(`${chalk.green(`Create a new Directory ${viewDir}.`)}`);
+            console.log();
+
+            fs.ensureDirSync(viewDir);
+        }
+
+        let name = "";
+        for (let idx = 0; names.length > idx; ++idx) {
+
+            const base = names[idx];
+
+            name += base
+                .charAt(0)
+                .toUpperCase() + base.slice(1);
+
+        }
+
+        // create View file
+        const viewFile = path.join(viewDir, `${name}View.js`);
+        if (!fs.existsSync(viewFile)) {
+
+            console.log(`Create a new View Class ${viewFile}.`);
+            console.log();
+
+            fs.writeFileSync(viewFile, `import { View } from "@next2d/framework";
+
+/**
+ * @class
+ * @extends {View}
+ */
+export class ${name}View extends View
+{
+    /**
+     * @constructor
+     * @public
+     */
+    constructor ()
+    {
+        super();
+    }
+}`);
+        }
+
+        // create ViewModel file
+        const viewModelFile = path.join(viewDir, `${name}ViewModel.js`);
+        if (!fs.existsSync(viewModelFile)) {
+
+            console.log(`Create a new ViewModel Class ${viewModelFile}.`);
+            console.log();
+
+            fs.writeFileSync(viewModelFile, `import { View, ViewModel } from "@next2d/framework";
+
+/**
+ * @class
+ * @extends {ViewModel}
+ */
+export class ${name}ViewModel extends ViewModel
+{    
+    /**
+     * @param  {View} view
+     * @return {void}
+     * @method
+     * @public
+     */
+    unbind (view: View)
+    {
+        console.log(view);
+    }
+
+    /**
+     * @param  {View} view
+     * @return {Promise}
+     * @method
+     * @public
+     */
+    bind (view: View): Promise<View>
+    {
+        return this
+            .factory(view)
+            .then((view) =>
+            {
+                return Promise.resolve(view);
+            });
+    }
+}`);
+        }
+    }
+
+    console.log(`${chalk.green("Finished.")}`);
+    console.log();
+};
+
+/**
+ * @return {void}
+ */
 const exec = function ()
 {
     new Command()
@@ -154,7 +279,11 @@ const exec = function ()
         .version(packageJson.version)
         .parse(process.argv);
 
-    createFile();
+    if (!fs.existsSync(`${process.cwd()}/src/index.ts`)) {
+        createTypeScriptFile();
+    } else {
+        createJavaScriptFile();
+    }
 };
 
 exec();
